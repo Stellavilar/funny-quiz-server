fb7ca12b006707ca485383a4a92377d8b6ce58a3
Stella Vilar <vilar.stella@gmail.com>
2021-03-12 17:58:55 +0100

    Update cors middleware

--- server/middlewares/corsMiddleware.js
+++ server/middlewares/corsMiddleware.js
@@ -2,7 +2,7 @@
 
 const corsMiddleware = (req, res, next) => {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', 'https://stellavilar.github.io/funny-quiz-client/#/', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Access-Control-Allow-Headers, Authorization, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
     if (req.method === "OPTIONS") {
